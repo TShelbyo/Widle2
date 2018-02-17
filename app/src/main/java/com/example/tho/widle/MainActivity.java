b6697f8b961e7f9970caede8945b14949f03ff9c
Théo Quessandier <theo.quessandier@wanadoo.fr>
2018-02-17 14:28:06 +0100

    paul génie du XXIeme siecle

--- app/src/main/java/com/example/tho/widle/MainActivity.java
+++ app/src/main/java/com/example/tho/widle/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.tho.widle;
 
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,10 +9,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +26,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<String> mAdapter2;
     private ActionBarDrawerToggle mDrawerToggle;
     private String mActivityTitle;
-
+    String[] ArrayA = { "Salle 2127", "Salle 2237", "Salle 2235"};
+    String[] Array2 ={"Disponible","Non disponible","Bientot indiponible"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +38,28 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList = (ListView) findViewById(R.id.myList);
         final Spinner sp=(Spinner) findViewById(R.id.spinner);
         final Button b=(Button) findViewById(R.id.button1);
+
         listeBase =(ListView) findViewById(R.id.myList2);
-        addItems2();
+
+        //CustomListView customListView=new CustomListView();
+
+        mAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ArrayA);
+        listeBase.setAdapter(mAdapter2);
+        //addItems2();
         ArrayAdapter<String> monAdapteur= new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.spinner));
         monAdapteur.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp.setAdapter(monAdapteur);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MainRessource.class);
+                startActivity(intent);
+
+            }
+
+        });
 
 
         addItems();
@@ -64,12 +85,13 @@ public class MainActivity extends AppCompatActivity {
         mDrawerList.setAdapter(mAdapter);
     }
 
-    private void addItems2() {
+    /*private void addItems2() {
         String[] Array = { "Salle 2127", "Salle 2237", "Salle 2235"};
-        mAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Array);
-        listeBase.setAdapter(mAdapter2);
+        String[] Array2 ={"Disponible","Non disponible","Bientot indiponible"};
+        //mAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Array);
+        listeBase.setAdapter(customListView);
         //listeBase.getChildAt(1).setBackgroundColor(Color.parseColor("#FF4081"));
-    }
+    }*/
 
 
     private void setupDrawer() {
@@ -112,5 +134,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    class CustomListView extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view=getLayoutInflater().inflate(R.layout.list,null);
+            TextView t1=(TextView) findViewById(R.id.textRessource);
+            TextView t2=(TextView) findViewById(R.id.textViewRessource2);
+            t1.setText(ArrayA[position]);
+            t2.setText(Array2[position]);
+
+            return view;
+        }
     }
 }
